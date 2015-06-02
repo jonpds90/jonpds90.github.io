d81ef6fd7a179a5a4e9f546b1d78dcb1b7db71d2
jonpds90 <jonpds90@gmail.com>
2015-06-02 10:25:04 -0400

    fixing email

--- send_form_email.php
+++ send_form_email.php
@@ -142,14 +142,8 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- include your own success html here -->
  
- 
- 
-Thank you for contacting us. We will be in touch with you very soon.
- 
- 
- 
 <?php
- 
+header('Location:http://jonpds90.github.io/') 
 }
  
 ?>
